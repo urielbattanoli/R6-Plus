09ee9bc3c10b39a86358515d7b1f54a0634eb650
Uriel Battanoli <urielbattanoli@gmail.com>
2018-04-13 19:20:58 -0300

    Initial commit

---
+++ R6-Plus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  R6-Plus
+//
+//  Created by Uriel Battanoli on 13/04/18.
+//  Copyright © 2018 Mocka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
