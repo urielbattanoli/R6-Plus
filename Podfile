efb96281710e15f0417afc7200f0f9e8fa0a89aa
Uriel Battanoli <urielbattanoli@gmail.com>
2018-07-31 23:15:40 -0300

    Adding onesignal for push notifications

--- Podfile
+++ Podfile
@@ -2,9 +2,18 @@ source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '10.0'
 use_frameworks!
 
+def shared_pods
+    pod 'OneSignal', '>= 2.5.2', '< 3.0'
+end
+
 target 'R6-Plus' do
     pod 'Alamofire', '~> 4.7'
     pod 'Kingfisher', '~> 4.0'
     pod 'Google-Mobile-Ads-SDK'
     pod 'Firebase/Core'
+    shared_pods
+end
+
+target 'OneSignalNotificationServiceExtension' do
+    shared_pods
 end
--- R6-Plus/AppDelegate.swift
+++ R6-Plus/AppDelegate.swift
@@ -9,12 +9,14 @@
 import UIKit
 import GoogleMobileAds
 import Firebase
+import OneSignal
 
 let ADS_APP_ID = "ca-app-pub-3291479380654020~6866270802"
 let ADS_BANNER_ID = "ca-app-pub-3291479380654020/4653210435"
 let ADS_VIDEO_ID = "ca-app-pub-3291479380654020/3432037228"
 //let ADS_BANNER_ID = "ca-app-pub-3940256099942544/2934735716" //test
 //let ADS_VIDEO_ID = "ca-app-pub-3940256099942544/1712485313" //test
+let ONESIGNAL_APP_ID = ""
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -29,6 +31,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                                     withAdUnitID: ADS_VIDEO_ID)
         ReviewHelper.incrementAppOpenedCount()
         AnalitycsHelper.AppOpened.logEvent(obs: "\(R6UserDefaults.shared.openCount)")
+        
+        let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
+        OneSignal.initWithLaunchOptions(launchOptions,
+                                        appId: ONESIGNAL_APP_ID,
+                                        handleNotificationAction: nil,
+                                        settings: onesignalInitSettings)
+        OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification
+        application.registerForRemoteNotifications()
+        OneSignal.promptForPushNotifications() { accepted in
+            print("User accepted notifications: \(accepted)")
+        }
         
         return true
     }
